8be2adbeb8a149a8fe98a1a854b64a9724e13dbf
Haroon <106879583+sky0walker99@users.noreply.github.com>
2024-10-02 21:18:01 +0530

    Final Commit test 3

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -139,4 +139,4 @@ def get_chat_history():
 
 # Start the server
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True)
--- start.sh
+++ start.sh
@@ -1,13 +1,3 @@
-# Navigate to the Frontend folder and install npm dependencies
-echo "Installing npm dependencies in the Frontend directory..."
-cd Frontend || { echo "Frontend directory not found"; exit 1; }
-npm install
-
-# Start the frontend
-npm run dev &
-
-# Go back to the root directory
-cd ..
 
 # Start the backend
 echo "Starting the backend..."
