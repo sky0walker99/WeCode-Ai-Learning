f8f673d3dca0a83b0aba271b7cb9bf92f569aa6a
Haroon <106879583+sky0walker99@users.noreply.github.com>
2024-10-02 20:32:54 +0530

    Final Commit test

---
+++ start.sh
@@ -0,0 +1,10 @@
+# Navigate to the Frontend folder and install npm dependencies
+echo "Installing npm dependencies in the Frontend directory..."
+cd Frontend || { echo "Frontend directory not found"; exit 1; }
+npm install
+
+
+# Run both frontend and backend using concurrently
+echo "Starting both frontend and backend..."
+cd Frontend
+npx concurrently "npm run dev" "python ./backend/src/main.py"
